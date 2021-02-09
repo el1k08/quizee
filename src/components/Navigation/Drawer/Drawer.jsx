0f1c421dc72711768a88d3dab07174b2bc2fa763
Eli Slavutski <eli.slavutski@gmail.com>
2021-02-09 19:57:24 +0200

    routing navigation

--- src/components/Navigation/Drawer/Drawer.jsx
+++ src/components/Navigation/Drawer/Drawer.jsx
@@ -2,18 +2,33 @@
 import React from 'react';
 import classes from './Drawer.module.scss'
 import Backdrop from '../../UI/Backdrop/Backdrop'
+import { NavLink } from 'react-router-dom'
 
-const links = [1, 2 , 3]
+const links = [
+  {to: '/', label: 'Список', exact: true},
+  {to: '/auth', label: 'Авторизация', exact: false},
+  {to: '/quiz-creator', label: 'Создать тест', exact: false}
+]
 
 class Drawer extends React.Component {
   state = {  }
 
+  clickHandler = () => {
+    this.props.onClose()
+  }
 
   renderLinks() {
     return links.map((link, index) => {
       return(
         <li key={index}>
-          <a> Link {link} </a>
+          <NavLink
+            to={link.to}
+            exact={link.exact}
+            activeClassName={classes.active}
+            onClick={this.clickHandler}
+          > 
+          {link.label}
+          </NavLink>
         </li>
       )
     })
